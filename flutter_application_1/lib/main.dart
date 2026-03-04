456eda2dd22e9168770e1c56080c09ede26729fb
DamarWisnuAngjaya <menoxdwa@gmail.com>
2026-03-04 10:37:45 +0700

    nrp

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Damar Wisnu Angjaya'),
+      home: const MyHomePage(title: 'Damar Wisnu Angjaya - 3124521050'),
     );
   }
 }
